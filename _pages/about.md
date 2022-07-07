a2ce961146e99b6e0355429b0b2a786e4923ad47
Tyler Etters <tyler.etters@output.com>
2022-07-07 17:34:35 -0500

    initial build

--- _pages/about.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: about
-permalink: /about
----
-# About
-
-[This theme](https://github.com/tyleretters/perfect-bootstrap-jekyll) was created by Tyler Etters at [Northern Information](https://nor.the-rn.info).
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,57 @@
-console.log('script.js loaded. see assets/javascript/script.js');
+'use strict';
+
+var wavesurfer1;
+document.addEventListener('DOMContentLoaded', function() {
+  wavesurfer1 = WaveSurfer.create({
+    container: document.querySelector('#waveform1'),
+    waveColor: '#0000FF',
+    progressColor: '#CCCCCC',
+    vertical: true
+  });
+  wavesurfer1.on('error', function(e) {
+      console.warn(e);
+  });
+
+  wavesurfer1.load('/assets/audio/Recorded_Audio86.wav');
+  document
+    .querySelector('[data-action="play1"]')
+    .addEventListener('click', wavesurfer1.playPause.bind(wavesurfer1));
+});
+
+var wavesurfer2;
+document.addEventListener('DOMContentLoaded', function() {
+  wavesurfer2 = WaveSurfer.create({
+    container: document.querySelector('#waveform2'),
+    waveColor: '#0000FF',
+    progressColor: '#CCCCCC',
+    vertical: true
+  });
+  wavesurfer2.on('error', function(e) {
+      console.warn(e);
+  });
+
+  wavesurfer2.load('/assets/audio/Recorded_Audio82.wav');
+  document
+    .querySelector('[data-action="play2"]')
+    .addEventListener('click', wavesurfer2.playPause.bind(wavesurfer2));
+});
+
+var wavesurfer3;
+document.addEventListener('DOMContentLoaded', function() {
+  wavesurfer3 = WaveSurfer.create({
+    container: document.querySelector('#waveform3'),
+    waveColor: '#0000FF',
+    progressColor: '#CCCCCC',
+    vertical: true
+  });
+  wavesurfer3.on('error', function(e) {
+      console.warn(e);
+  });
+
+  wavesurfer3.load('/assets/audio/Recorded_Audio80.wav');
+  document
+    .querySelector('[data-action="play3"]')
+    .addEventListener('click', wavesurfer3.playPause.bind(wavesurfer3));
+});
+
+
